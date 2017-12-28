6051e88ccca58c152160625755bbf37086474409
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-12-27 21:48:17 -0800

    initial commit

---
+++ snippets.py
@@ -0,0 +1,42 @@
+import rumps
+import xerox
+import os
+
+rumps.debug_mode = True
+
+datafile = "data.txt"
+
+
+def copy(text):
+    print text.title
+    xerox.copy(text.title)
+
+def add(_):
+    print "add clicked"
+    print xerox.paste()
+    with open(datafile, "a") as f:
+        f.write(xerox.paste() + "\n")
+    app.menu(generate_menu)
+    # print type(rumps.MenuItem.update())
+    rumps.notification("Awesome title", "amazing subtitle", "hi!!!")
+
+
+def generate_menu():
+    menu = [rumps.MenuItem("add", callback=add)]
+    date = []
+
+    if os.path.isfile(datafile):
+        with open(datafile) as f:
+            data = f.read().strip().splitlines()
+    else:
+        open(datafile, "w").write("")
+
+    for line in data:
+        menu.append(rumps.MenuItem(line, callback=copy))
+
+    return menu
+
+if __name__ == "__main__":
+    app = rumps.App(" S ", quit_button="quit")
+    app.menu = generate_menu()
+    app.run()
